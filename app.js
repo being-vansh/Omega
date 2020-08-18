ed2c4f32359ff366ca230ec88424fc801ad99cbb
being-vansh <69839778+being-vansh@users.noreply.github.com>
2020-08-18 13:05:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCqLLatqmC3RkrRevK-pKzyQZHq9yqMdxs",
+  authDomain: "dauntless-sun-280716.firebaseapp.com",
+  databaseURL: "https://dauntless-sun-280716.firebaseio.com",
+  projectId: "dauntless-sun-280716",
+  storageBucket: "dauntless-sun-280716.appspot.com",
+  messagingSenderId: "1068355504268",
+  appId: "1:1068355504268:web:2641366815cf8bb06f53a9",
+  measurementId: "G-W6M8HFK5FM"
 };
-
-     
- 
 ////Do Not Edit Below This
 
 firebase.initializeApp(firebaseConfig);
